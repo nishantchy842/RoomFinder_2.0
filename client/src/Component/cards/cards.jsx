78be36f033c81abf3e7420fc95844e7330052c81
Nishant chaudhary <nishantchaudhary842@gmail.com>
2023-05-18 13:42:52 +0545

    card testing

--- client/src/Component/cards/cards.jsx
+++ client/src/Component/cards/cards.jsx
@@ -1,40 +1,26 @@
-import { useEffect, useState } from "react";
-import "./cards.css";
-import axios from "axios";
-import moment from "moment/moment";
 import { useNavigate } from "react-router";
+import "./cards.css";
+import PropTypes from "prop-types";
 
-const Cards = () => {
-  const navigate = useNavigate();
-  const [room, setRoom] = useState();
-  const getAllRooms = async () => {
-    const res = await axios("http://localhost:8000/api/room/room");
-    setRoom(res.data.rooms);
-  };
-  useEffect(() => {
-    getAllRooms();
-  }, []);
+const Cards = ({ item }) => {
+const navigate = useNavigate()
   return (
-    <div>
-      <div className="cards__wrapper">
+    <div >
+      <div className="cards__wrapper cursor-pointer" onClick={()=>navigate(`/Room/${item.title}`,{state: item})}>
         <div className="cards__room-img">
-          <img src="http://bit.ly/2tMBBTd" height={420} width={327} />
+          <img src={item.img_collection[0]} height={420} width={327} />
         </div>
         <div className="cards__room-info">
           <div className="cards__room-text">
-            <h1>Room In Satdobato</h1>
+            <h1>{item.title}</h1>
             <h2>Satodobato, Lalitpur, Nepal</h2>
             <p>
-              Harvest Vases are a reinterpretation
-              <br /> of peeled fruits and vegetables as
-              <br /> functional objects. The surfaces
-              <br /> appear to be sliced and pulled aside,
-              <br /> allowing room for growth.{" "}
+              {item.description}
             </p>
           </div>
           <div className="cards__room-price">
             <p>
-              NRs. <span>21000</span>
+              NRs. <span>{item.price}</span>
             </p>
           </div>
         </div>
@@ -42,5 +28,7 @@ const Cards = () => {
     </div>
   );
 };
-
+Cards.propTypes = {
+  item:  PropTypes.object,
+};
 export default Cards;
