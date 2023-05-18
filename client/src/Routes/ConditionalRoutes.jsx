8e5c2b349f24fdf3646e851d54db5ab5fb891e55
Swarup Sapkota <empswarupcoc@gmail.com>
2023-05-18 13:19:29 +0545

    cards

--- client/src/Routes/ConditionalRoutes.jsx
+++ client/src/Routes/ConditionalRoutes.jsx
@@ -1,29 +1,30 @@
-import { Route, Routes } from "react-router"
-import AddLocation from "../Container/addLocation/AddLocation"
-import Home from "../Container/Home/Home"
-import AddDetails from "../Container/addDetails/AddDetails"
-import AddRoom from "../Container/AddRoom"
-import Register from "../Container/auth/Register"
-import Login from "../Container/auth/Login"
-import RoomCard from '../Component/cards/card'
-import RespRoom from "../pages/RespectiveRoom/RespRoom"
+import { Route, Routes } from "react-router";
+import AddLocation from "../Container/addLocation/AddLocation";
+import Home from "../Container/Home/Home";
+import AddDetails from "../Container/addDetails/AddDetails";
+import AddRoom from "../Container/AddRoom";
+import Register from "../Container/auth/Register";
+import Login from "../Container/auth/Login";
+import RoomCard from "../Component/cards/card";
+import RespRoom from "../pages/RespectiveRoom/RespRoom";
+import Cards from "../Component/cards/cards";
 
 const ConditionalRoutes = () => {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/add-details" element={<AddDetails />} />
-      <Route path="/add-map" element={<AddLocation />} />
-      <Route path="/add-room" element={<AddRoom />} />
-      <Route path="/registration" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/card" element={<RoomCard />} />
-      <Route path="/Room/:slug" element={<RespRoom />} />
-      
+        <Route path="/" element={<Home />} />
+        <Route path="/add-details" element={<AddDetails />} />
+        <Route path="/add-map" element={<AddLocation />} />
+        <Route path="/add-room" element={<AddRoom />} />
+        <Route path="/registration" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/card" element={<RoomCard />} />
+        <Route path="/Room/:slug" element={<RespRoom />} />
+        <Route path="/cards" element={<Cards />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default ConditionalRoutes
+export default ConditionalRoutes;
