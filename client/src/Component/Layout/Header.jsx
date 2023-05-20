efd70c15f9b9ffdaea69f175a81847e6f91e7f86
Swarup Sapkota <empswarupcoc@gmail.com>
2023-05-20 12:44:10 +0545

    bugs

--- client/src/Component/Layout/Header.jsx
+++ client/src/Component/Layout/Header.jsx
@@ -42,64 +42,69 @@ const Header = () => {
   };
 
   return (
-    <div className="shadow-md w-full fixed top-0 left-0 z-20 bg-[#295d61]">
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
-        <div
-          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800"
-          onClick={() => navigate("/")}
-        >
-          <span className="text-3xl text-indigo-600 mr-1 pt-2">
-            <ion-icon name="logo-ionic"></ion-icon>
-          </span>
-          RoomFinder
-        </div>
-
-        {isLoggedIn ? (
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar
-                alt="Remy Sharp"
-                src={`${
-                  import.meta.env.VITE_APP_URL
-                }/uploads/${userProfilePicture}`}
-                sx={{ width: 50, height: 50 }}
-              >
-                {" "}
-              </Avatar>
-            </IconButton>
-          </Tooltip>
-        ) : (
-          <button className="btn" onClick={() => navigate("/login")}>
-            LOGIN
-          </button>
-        )}
-
-        <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#295d61] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 " : "top-[-490px]"
-          }`}
-        >
-          {Links.map((link) => (
-            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-              <Link
-                to={link.link}
-                className="text-gray-800 hover:text-gray-400 duration-500"
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-          <button
-            className="btn text-white py-2 px-6 rounded md:ml-8
-    duration-500"
-            onClick={() => navigate("/login")}
+    <AppBar position="static">
+      <Container
+        maxWidth="xl"
+        className="shadow-md w-full fixed top-0 left-0 z-20 bg-[#f7f7f7] p-2"
+      >
+        <Toolbar disableGutters className="flex justify-between">
+          <div
+            className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins]
+    text-gray-800"
+            onClick={() => navigate("/")}
           >
-            LOGIN
-          </button>
-        </ul>
-      </div>
-    </div>
+            <span className="text-3xl text-indigo-600 mr-1 pt-2">
+              <ion-icon name="logo-ionic"></ion-icon>
+            </span>
+            <span className="font-sans text-2xl ml-2">RoomFinder</span>
+          </div>
+          <Box sx={{ flexGrow: 0 }}>
+            {isLoggedIn ? (
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={`${
+                      import.meta.env.VITE_APP_URL
+                    }/uploads/${userProfilePicture}`}
+                    sx={{ width: 50, height: 50 }}
+                  >
+                    {" "}
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <button className="btn" onClick={() => navigate("/login")}>
+                LOGIN
+              </button>
+            )}
+
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 export default Header;
--- client/src/Component/cards/card.jsx
+++ client/src/Component/cards/card.jsx
@@ -1,29 +1,27 @@
-
-import { useEffect, useState } from 'react'
-import './card.css'
-import axios from 'axios'
-import Layout from '../Layout/Layout'
-import Cards from './cards'
-
+import { useEffect, useState } from "react";
+import "./card.css";
+import axios from "axios";
+import Layout from "../Layout/Layout";
+import Cards from "./cards";
 
 const RoomCard = () => {
-    const [room, setRoom] = useState()
-    const getAllRooms = async () => {
-        const res = await axios('http://localhost:8000/api/room/room')
-        setRoom(res.data.rooms)
-    }
-    useEffect(() => {
-        getAllRooms()
-    }, [])
-    return (
-        <Layout>
-            <div className='flex flex-wrap justify-center gap-3 flex-shrink-0 mt-20'>
-                {
-                    room?.map((item) => {
-                        return (
-                            <Cards  key={item._id} item={item} />
-                        )
-                    })
-                }
+  const [room, setRoom] = useState();
+  const getAllRooms = async () => {
+    const res = await axios("http://localhost:8000/api/room/room");
+    setRoom(res.data.rooms);
+  };
+  useEffect(() => {
+    getAllRooms();
+  }, []);
+  return (
+    <Layout>
+      <div className="flex flex-wrap justify-center gap-3 flex-shrink-0 mt-20">
+        {room?.map((item) => {
+          return <Cards key={item._id} item={item} />;
+        })}
+      </div>
+    </Layout>
+  );
+};
 
 export default RoomCard;
