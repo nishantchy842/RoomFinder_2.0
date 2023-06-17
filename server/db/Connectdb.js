9e179a9c5e4e87937287d668badeb3650803dada
Swarup Sapkota <empswarupcoc@gmail.com>
2023-06-17 08:55:12 +0545

    spsps

--- server/db/Connectdb.js
+++ server/db/Connectdb.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-
-const connectDb = async () =>{
-    try{
-        const data = mongoose.connect('mongodb://localhost:27017/RoomFinder')
-        if(data){
-            console.log('Database is successfull connected')
-        }
-    }catch(error){
-        console.log("Data base is not connected", error)
+const connectDb = async () => {
+  try {
+    const data = mongoose.connect("mongodb://localhost:27017/RoomFinder");
+    if (data) {
+      console.log("Database is successfull connected");
     }
-}
+  } catch (error) {
+    console.log("Data base is not connected", error);
+  }
+};
 
-module.exports = connectDb
+module.exports = connectDb;
